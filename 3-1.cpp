5a20fe371cd23706ece414398f864d5f3823946b
liuyvxin52 <xinxintpai@outlook.com>
2023-08-20 14:01:09 +0800

    modified:   3-1.cpp

--- 3-1.cpp
+++ 3-1.cpp
@@ -1,2 +1,19 @@
 //1.
-#include <
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int Maxn = /*1->*//*<-1*/;
+int n, a[Maxn];
+long long s[Maxn], ans;
+
+bool check(int l, int r){
+    if(s[r] - s[l - 1] == /*2->*//*<-2*/) return true;
+    return false;
+}
+
+
+
+int main(){
+    
+}
